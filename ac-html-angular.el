6bafe09afe03112ca4183d58461c1a6f6c2b3c67
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-12-25 09:19:33 +0200

    minor comment

--- ac-html-angular.el
+++ ac-html-angular.el
@@ -25,6 +25,10 @@
 
 ;; Angular 1.5 completion with doc.
 ;;
+;; Install and configure one of next packages:
+;; 
+;; `ac-html' or `company-web'
+;;
 ;; Usage:
 ;;
 ;; Use `ac-html-angular1+' or `company-web-angular1+'
